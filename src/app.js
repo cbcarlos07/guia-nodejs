508e95f745ef6cdcb7ff1328567b3e37c3070d0a
cbcarlos07 <cbcarlos08@gmail.com>
2022-05-11 01:21:40 -0400

    Capítulo 3

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
-app.get('/', function(req, res) {
-  res.send('hello world');
+app.get('/', (req, res) => res.send({mensagem: 'Olá mundo!'}));
+
+app.listen(3000, ()=> {
+  console.log('API rodando na porta 3000');
 });
-
-app.listen(3000);
