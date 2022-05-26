8628463b3e3c11feacd9fb3b13cbfbd1cb11a5d9
cbcarlos07 <cbcarlos08@gmail.com>
2022-05-25 23:45:08 -0400

    Capitulo 4

--- src/app.js
+++ src/app.js
@@ -2,5 +2,29 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => res.send({mensagem: 'Olá mundo!'}));
+app.get('/soma-por-dois', (req, res) => {
+    res.json({resultado: 2 + 2})
+});
+app.get('/soma/:primeiro/:segundo', (req, res) => {
+    const {primeiro, segundo} = req.params
+    res.json({resultado: parseInt(primeiro) + parseInt(segundo)})
+});
+
+app.get('/multi/:primeiro/:segundo', (req, res) => {
+    const { primeiro, segundo } = req.params;
+    res.json({resultado: parseInt(primeiro) * parseInt(segundo)})
+});
+
+app.get('/dividir/:primeiro/:segundo', (req, res) => {
+    const { primeiro, segundo } = req.params;
+    res.json({resultado: parseInt(primeiro) / parseInt(segundo)})
+});
+
+app.get('/sub/:primeiro/:segundo', (req, res) => {
+    const { primeiro, segundo } = req.params;
+    res.json({resultado: parseInt(primeiro) - parseInt(segundo)})
+});
+
+
 
 app.listen(3000, () => console.log('API rodando na porta 3000'));
