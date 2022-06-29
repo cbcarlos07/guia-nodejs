4ab0ec5e7f207c406703f9b0542fa7b12d2e896b
cbcarlos07 <cbcarlos08@gmail.com>
2022-06-29 16:32:01 -0400

    rota de calculo

--- src/routes/calcRoutes.js
+++ src/routes/calcRoutes.js
@@ -5,19 +5,10 @@ const router = express.Router()
 router.get('/soma-por-dois', calcController.somaPorDoisController)
 router.get('/soma/:primeiro/:segundo', calcController.somaPrimeiroPeloSegundoController);
 
-router.get('/multi/:primeiro/:segundo', (req, res) => {
-    const { primeiro, segundo } = req.params;
-    res.json({resultado: parseInt(primeiro) * parseInt(segundo)})
-});
+router.get('/multi/:primeiro/:segundo', calcController.multiPrimeiroPeloSegundo);
 
-router.get('/dividir/:primeiro/:segundo', (req, res) => {
-    const { primeiro, segundo } = req.params;
-    res.json({resultado: parseInt(primeiro) / parseInt(segundo)})
-});
+router.get('/dividir/:primeiro/:segundo', calcController.dividirPrimeiroPeloSegundo);
 
-router.get('/sub/:primeiro/:segundo', (req, res) => {
-    const { primeiro, segundo } = req.params;
-    res.json({resultado: parseInt(primeiro) - parseInt(segundo)})
-});
+router.get('/sub/:primeiro/:segundo', calcController.subPrimeiroPeloSegundo);
 
 module.exports = router
