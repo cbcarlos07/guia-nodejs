596ab290753cc01d3812c2e738b3e930f3e99b17
cbcarlos07 <cbcarlos08@gmail.com>
2022-07-04 17:35:36 -0400

    capitulo 13

--- src/routes/calcRoutes.js
+++ src/routes/calcRoutes.js
@@ -4,11 +4,8 @@ const router = express.Router()
 
 router.get('/soma-por-dois', calcController.somaPorDoisController)
 router.get('/soma/:primeiro/:segundo', calcController.somaPrimeiroPeloSegundoController);
-
 router.get('/multi/:primeiro/:segundo', calcController.multiPrimeiroPeloSegundo);
-
 router.get('/dividir/:primeiro/:segundo', calcController.dividirPrimeiroPeloSegundo);
-
 router.get('/sub/:primeiro/:segundo', calcController.subPrimeiroPeloSegundo);
 
 module.exports = router
